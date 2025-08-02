0eda6429e8905c6fe988439bba77150bb913fb69
Niespodzianek <64963974+Niespodzianek@users.noreply.github.com>
2025-08-02 03:16:14 +0000

    Rozpoczęcie pracy nad programem

---
+++ main.py
@@ -0,0 +1,11 @@
+import yfinance as yt
+
+skrot = "AAPL"
+try:
+    spolka = yt.Ticker(skrot)
+    notowania = spolka.history(period="max")
+    print(notowania)
+except Exception:
+    print(f"Nie udało się pobrać danych dla spółki: {skrot} !!!")
+
+print(f"noto {notowania}")
